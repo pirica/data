48a9b451b41d63aa62e8d2a2d9a45c5a43186649
blobry <blobry@users.noreply.github.com>
2020-11-26 13:00:44 -0800

    Update index.js

--- item/index.js
+++ item/index.js
@@ -3,7 +3,7 @@ const id = window.location.pathname.substring(window.location.pathname.lastIndex
 (async () => {
     try {
         const item = await (await fetch(`https://api.blobry.com/data/items/${id}`)).json();
-        $('body')[0].innerHTML += item.widget.usage.shop;
+        console.log(item);
     } catch(err) {
         $('path').css('fill', '#6175B7');
         $('.message').html('An error occurred, check console for more information.');
