8a65a1468ca550ffe9156c4f5a17a77ec99740ca
blobry <blobry@users.noreply.github.com>
2020-11-25 23:20:46 -0800

    ok

--- redirect/index.js
+++ redirect/index.js
@@ -9,6 +9,9 @@ const exist = async (path) => {
 };
 
 (async () => {
+    console.log(pathname.split('/'))
+    console.log(pathname.split('/').length)
+    if(pathname.split('/').length < 2) return;
     const path = await exist('../index.html');
     if(path) console.log('POGGERS this directory exists!!!!!!!!!!!!!!!!!!!!!!!!!!');
     else console.log('NO POOGGERS NOOOOOO')
